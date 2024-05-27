34a88a95ef4beb4a6188dd830473508b5294f529
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-05-27 18:59:26 +0200

    rettangolo.java

--- src/rettangolo.java
+++ src/rettangolo.java
@@ -1,14 +1,14 @@
-public class rettangolo extends forma{
+public class Rettangolo extends Forma{
 
     private double lunghezza;
     private double larghezza;
-
+    //override del metodo calcolo area creato su forma
     @Override
     public void calcolaArea() {
-       double area = lunghezza * larghezza;
-       System.out.println("l'area è: " + area);
+        double area = lunghezza * larghezza;
+        System.out.println("l'area è: " + area);
     }
-    public  rettangolo(double lunghezza, double larghezza) {
+    public  Rettangolo(double lunghezza, double larghezza) {
         this.lunghezza = lunghezza;
         this.larghezza = larghezza;
     }
